78ecc9f2544ce4f4cd4068baf480cbc3c9994b9b
Disttack <josephabbottiii@gmail.com>
2024-12-11 13:43:13 -0700

    added to nested if

--- 100-days-of-python-challenge/nested_if_ticket.py
+++ 100-days-of-python-challenge/nested_if_ticket.py
@@ -1,13 +1,24 @@
 print("Welcome to the rollercoaster!")
 height = int(input("How tall are you in inches?"))
+bill = 0
+
 if height >= 60:
     print("You may ride!")
     age = int(input("How old are you?"))
     if age >= 18:
-        print("That will be 10 dollars")
+        print("Adult tickets will be 10 dollars")
+        bill = 10
     elif age >= 14:
-        print("That will be 7 dollars")
+        print("Youth tickets will be 7 dollars")
+        bill = 7
     else:
-        print("That will be 5 dollars")
+        print("Child tickets will be 5 dollars")
+        bill = 5
+    
+    wants_photo = input("Would you like your photo taken? Type Y for yes and N for no.")
+    if wants_photo == "Y":
+        bill += 3
+        # += is the same as bill = bill + 3
+    print(f"Your final bill is ${bill}")
 else:
     print("You will need to wait until you are taller!")
