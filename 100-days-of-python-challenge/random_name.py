282fe848ecd25aa1cfc6226106eeecb7f4935895
Disttack <josephabbottiii@gmail.com>
2024-12-14 22:25:16 -0700

    condensed

--- 100-days-of-python-challenge/random_name.py
+++ 100-days-of-python-challenge/random_name.py
@@ -1,13 +1,3 @@
 import random
 friends = ["Alice", "Bob", "Charlie", "David", "Emmanual"]
-random_int = random.randint(a=1,b=5)
-if random_int == 1:
-    print(friends[0])
-elif random_int == 2:
-    print(friends[1])
-elif random_int == 3:
-    print(friends[2])
-elif random_int == 4:
-    print(friends[3])
-else:
-    print(friends[4])
+print(random.choice(friends))
