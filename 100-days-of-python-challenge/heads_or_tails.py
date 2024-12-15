28382d1d60fdb960814f991527be29c33d1d33e3
Disttack <josephabbottiii@gmail.com>
2024-12-14 21:48:53 -0700

    random numbers

---
+++ 100-days-of-python-challenge/heads_or_tails.py
@@ -0,0 +1,7 @@
+import random
+
+flip = random.randint(a=0,b=1)
+if flip == 1:
+    print("Heads")
+else:
+    print("Tails")
---
+++ 100-days-of-python-challenge/random_numbers.py
@@ -0,0 +1,8 @@
+import random
+
+random_integer = random.randint(a= 1, b= 10)
+print(random_integer)
+
+#multiplication is how you increase the upper limit * 10 creates 0.0 -> 10.0
+random_floating = random.random() * 10
+print(random_floating)
